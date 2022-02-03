8e17a3e7bf6480d4a1e90c0962a9f40088ceece4
JohnBanares <jmbanares02@gmail.com>
2022-02-03 10:07:45 +0000

    labtut

--- java/src/ie/tudublin/BugZap.java
+++ java/src/ie/tudublin/BugZap.java
@@ -9,13 +9,29 @@ public class BugZap extends PApplet
 	{
 		size(500, 500);
 	}
-
+	float playerX, playerY, playerWidth;
 	public void setup() {
-		
+		colorMode(HSB);
+
+		playerX = width / 2;
+		playerY = height * 50;
+		playerWidth = 50;
+
+		restBug();
 	}
 	
+	private void restBug() 
+	{
+	
+	}
+
+	void drawPlayer(float x, float y, float w)
+	{
+
+	}
 	public void draw()
 	{	
-        
+        background(0);
+		drawPlayer(playerX, playerY, playerWidth);
 	}
 }
