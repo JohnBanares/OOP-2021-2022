eee8754f0eab7d7cbc362cc7b2485ed4579f1e16
JohnBanares <jmbanares02@gmail.com>
2022-02-21 11:39:01 +0000

    monday

---
+++ java/src/ie/tudublin/arrays.java
@@ -0,0 +1,24 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class arrays extends PApplet {
+    float[] rainfall = { 45, 37, 55, 27, 38, 58, 79, 48, 104, 31, 100, 58};
+
+    
+	public void settings()
+	{
+		size(500, 500);
+	}
+
+	public void setup() 
+    {
+	
+	}
+
+	
+	public void draw()
+	{	
+		
+	}
+}
