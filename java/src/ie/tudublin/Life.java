4b0b743e14e1352fe47f9bebd8534fb2c70ae96c
JohnBanares <jmbanares02@gmail.com>
2022-03-07 12:02:49 +0000

    mondayweek7

--- java/src/ie/tudublin/Life.java
+++ java/src/ie/tudublin/Life.java
@@ -4,16 +4,23 @@ import processing.core.PApplet;
 
 public class Life extends PApplet {
 
+	
+	LifeBoard board;
+
 	public void settings() {
 		size(500, 500);
+		board = new LifeBoard(100, this);
+		board.randomise();
 	}
 
 	public void setup() {
-		colorMode(HSB);
+		colorMode(RGB);
 
 	}
 
 	
 	public void draw() {
+		board.render();
 	}
 }
+//monday week7
